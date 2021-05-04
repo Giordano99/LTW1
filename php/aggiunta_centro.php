1a76a229fe2481ea115d34eebe8d71db733cf4bd
Giordano <giordano19991999@gmail.com>
2021-05-04 18:21:13 +0200

    ricerca improved

--- php/aggiunta_centro.php
+++ php/aggiunta_centro.php
@@ -20,36 +20,54 @@
     $descrizione = $_POST["descrizione"];
     $regole = $_POST["regole"];
 
-    $query = "insert into centroSportivo(nome, citta, indirizzo, descrizione, regole, docce, ristorante, bar, pizzeria, area_picnic, spogliatoi, casacche, pub, parcheggio, tornei_campionati, sala_feste, calcio_A5, beach_volley, calcio_A8, rugby, squash) values ('$nome','$citta','$indirizzo','$descrizione', '$regole', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)";
-    
-    if (mysqli_query($conn,$query)) {
+    $control = " select * from centroSportivo where nome = '$nome' and citta = '$citta' and indirizzo = '$indirizzo'";
 
-        $servizi = $_POST['servizi'];
-        $array = $servizi;
+    $risultato = mysqli_query($conn,$control);
+    $row = mysqli_fetch_array($risultato);
 
-        foreach($array as $key => $value) {
-    
-            $query2 = "update centroSportivo set $value = 1 where nome = '$nome' and citta = '$citta' and indirizzo = '$indirizzo';";            
-           
 
-            if (!mysqli_query($conn,$query2)) {
-                echo "<script>alert('op')</script>";
-            }
-        }
+    if($row){
         
-        $sport = $_POST['sport'];
+        #echo "utente già registrato";
+        
+        echo "<script>alert('Centro Sportivo già Registrato')</script>";
+        echo "<script>window.open('../Accesso/accesso.html','_self')</script>";
+        
+        
+        exit();
+    }
+    else {
 
-        foreach($sport as $key => $value) {
-            
-            $query3 = "update centroSportivo set $value = 1 where nome = '$nome' and citta = '$citta' and indirizzo = '$indirizzo';";            
-            if (mysqli_query($conn,$query3)) {
-            
-                echo "<script>alert('Centro Sportivo Aggiunto con Successo')</script>";
+        $query = "insert into centroSportivo(nome, citta, indirizzo, descrizione, regole, docce, ristorante, bar, pizzeria, area_picnic, spogliatoi, casacche, pub, parcheggio, tornei_campionati, sala_feste, calcio_A5, beach_volley, calcio_A8, rugby, squash) values ('$nome','$citta','$indirizzo','$descrizione', '$regole', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)";
+
+        if (mysqli_query($conn,$query)) {
+
+            $servizi = $_POST['servizi'];
+            $array = $servizi;
+
+            foreach($array as $key => $value) {
+
+                $query2 = "update centroSportivo set $value = 1 where nome = '$nome' and citta = '$citta' and indirizzo = '$indirizzo';";            
+                
+
+                if (!mysqli_query($conn,$query2)) {
+                    echo "<script>alert('op')</script>";
+                }
             }
-            else {
-            echo "<script>alert('op')</script>";}
+            
+            $sport = $_POST['sport'];
+
+            foreach($sport as $key => $value) {
+                
+                $query3 = "update centroSportivo set $value = 1 where nome = '$nome' and citta = '$citta' and indirizzo = '$indirizzo';";            
+                if (mysqli_query($conn,$query3)) {
+                
+                    echo "<script>alert('Centro Sportivo Aggiunto con Successo')</script>";
+                }
+                else {
+                echo "<script>alert('op')</script>";}
+            }
         }
     }
-
     echo "<script>window.location.href='../Accesso/accesso.html'</script>";
 ?>
