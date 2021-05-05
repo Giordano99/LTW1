bc5eda98b13a6f98cbbdd0a3e1a0b33ed126caf1
Giordano <giordano19991999@gmail.com>
2021-05-05 19:08:13 +0200

    roba

--- php/login.php
+++ php/login.php
@@ -1,34 +1,60 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "partitella";
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "partitella";
 
-$conn = new mysqli($host,$user,$pass,$db) or die("unable to connect");
-if(!$conn)
-{echo "connessione fallita";}
-echo "connessione al database effettuata";
+    $conn = new mysqli($host,$user,$pass,$db) or die("unable to connect");
 
-$pass = $_POST["password"];
-$mail = $_POST["email"];
+    if(!$conn) {
+    
+        echo "connessione fallita";
+    }
+    else {
+    
+        echo "connessione al database effettuata";
+    }
+    
 
-$query = "select nome from utente where email = '$mail' and password = '$pass'";
-if(mysqli_query($conn,$query)) {
+    //reset cookie
+    setcookie("mail","", time()-3600);
+    
+    $pass = $_POST["password"];
+    $mail = $_POST["email"];
 
-    setcookie("mail","$mail",strtotime("+1 year"));
-    echo "<script>alert('Login Effettuato con e-mail: $_COOKIE[mail]')</script>";
-    echo "<script>window.open('../Accesso/accesso.html#Home_Accesso','_self')</script>";
-    exit();    
-}
+    $pass = md5($pass);
+    
+    $query = "select nome from utente where email = '$mail' and password = '$pass'";
+    if(mysqli_query($conn,$query)) {
 
-else {
+        $risultato = mysqli_query($conn,$query);
+        #$row = mysqli_fetch_array($risultato);
 
-    echo "<script>alert('Accesso NEGATO')</script>";
-    echo "<script>window.open('../Index/index.html#Accedi-Registrati','_self')</script>";
-    exit();
-}
 
-echo "$query";
+        $rowcount = mysqli_num_rows($risultato);
+        if ($rowcount > 0) {
+            
+            setcookie("mail", $mail);
+            setcookie("mail", $mail, time()+3600);  /* expire in 1 hour */
+            echo "<script>alert('Login Effettuato con e-mail: $mail')</script>";
+            echo "<script>window.open('../Accesso/accesso.html#Home_Accesso','_self')</script>";
+            exit();  
+        }
+        else {
+
+            echo "<script>alert('Accesso NEGATO')</script>";
+            echo "<script>window.open('../Index/index.html#Accedi-Registrati','_self')</script>";
+            exit();
+        }
+    }
+    else {
+
+        echo "<script>alert('Problema di Connessione al Server')</script>";
+        echo "<script>window.open('../Index/index.html#Accedi-Registrati','_self')</script>";
+        exit();
+    }
+
+    echo "$query";
 
 ?>
