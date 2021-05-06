ad00f05a22244855c76b6b105f6bcd846670deab
Giuseppe Tallini <tallini.1814712@studenti.uniroma1.it>
2021-05-06 16:52:59 +0200

    prenotazioni elenco

---
+++ php/elenco_prenotazioni.php
@@ -0,0 +1,70 @@
+<?php
+
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "partitella";
+
+    $conn = new mysqli($host,$user,$pass,$db) or die("unable to connect");
+    if(!$conn) {
+
+        echo "connessione fallita";
+    }
+    
+    #echo "connessione al database effettuata";
+
+
+    $query = "select * from prenotazione where utente = '$_COOKIE[mail]'";
+    
+    
+    $risultato = mysqli_query($conn,$query);
+?>
+    <html>
+        <head>
+            <title> Partitella? - Ricerca </title>
+            <meta charset="utf-8"/>
+            <meta name="viewport" content="width=device-width initial-scale=1"/>
+
+            <link rel="stylesheet" type="text/css" href="../Index/index.css"/>
+
+            <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css"/>
+
+            <link rel="stylesheet" type="type/css" href="style.css"/>
+            <script type="text/javascript" lang="javascript"
+            src="../js/bootstrap.min.js"></script>
+            <!-- <script type="text/javascript" lang="javascript" src="script.js"></script> -->
+
+            <script type="text/javascript" src="../Index/index.js"></script>
+
+
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+        </head>
+
+        <body class="text-center">
+       
+        
+            <?php
+            while ($row = mysqli_fetch_array($risultato)) {
+
+                echo "<div>";
+                echo '<h2>'.$row['centroSportivoID'].'</h2>';
+                echo '<h4>'.$row['utente'].' -- '.$row['sport'].'</h4>';
+                
+            ?>
+        </body>
+        </html>
+        <?php
+            
+            }
+        ?>
+
+    </body>
+</html>
+
+
+  
+
+    
